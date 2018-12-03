4cca63505469e3930a5c4650508ffa9c6240008d
gujin <me450722457@vip.qq.com>
2018-12-03 14:13:25 +0800

    test-1

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-print('test')
-print('test2')
-print('李学海牛逼，李学海666')
+account = 'admin'
+password = '123456'
+
+user_account = input(str('Please input your account\n'))
+user_password = input(str('Please input your password\n'))
+
+if user_account == account:
+    if user_password == password:
+        print('success')
+    else:
+        print('password error')
+else:
+    print('account error')
